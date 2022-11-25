c95fa54117ee8fba86ec8ae425f59ed3f4342c3a
eking158 <eking158@naver.com>
2022-11-25 10:52:15 +0900

    11.25

--- main.js
+++ main.js
@@ -7,26 +7,26 @@ app.get('/', function(req,res) {
 
 // localhost:3000/main 브라우저에 res.sendFile() 내부의 파일이 띄워진다.
 app.get('/406', function(req,res){
-    res.sendFile(__dirname + "/public/storages/406.html");
+    res.sendFile(__dirname + "/public/storages/storages.html");
   })
 
 app.get('/storage_1', function(req,res){
-    res.sendFile(__dirname + "/public/storages/storage_1.html");
+    res.sendFile(__dirname + "/public/storages/storages.html");
   })
 
 app.get('/storage_2', function(req,res){
-    res.sendFile(__dirname + "/public/storages/storage_2.html");
-  })
-
-app.get('/json', function(req,res){
-    res.sendFile(__dirname + "/public/inventory_list_test_gui/json.html");
+    res.sendFile(__dirname + "/public/storages/storages.html");
 })
 
-app.get('/inventory/:id', function(req,res){
-  const {id} = req.params;
-  res.sendFile(__dirname + "/public/test/json_test.html");
+app.get('/406/:id', function(req,res){
+  res.sendFile(__dirname + "/public/test/inventory.html");
 })
-
+app.get('/storage_1/:id', function(req,res){
+  res.sendFile(__dirname + "/public/test/inventory.html");
+})
+app.get('/storage_2/:id', function(req,res){
+  res.sendFile(__dirname + "/public/test/inventory.html");
+})
 
 app.listen(3000, function(){
   console.log('Listening at 3000');
