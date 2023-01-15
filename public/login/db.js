cd5e745419440fff6c0d82ff9b5ee51646ac7a15
l0voj <wonjjw98@naver.com>
2023-01-16 02:28:41 +0900

    22/01/16:lightsail_apply=itemTable+userTable

--- public/login/db.js
+++ public/login/db.js
@@ -1,8 +1,8 @@
 var mysql = require('mysql2');
 var db = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'Robot1234!',
+    host: 'ls-44241f46297a5960bd783ef5c23f57ed8a10e7c3.cqzmsctc2nun.ap-northeast-2.rds.amazonaws.com',
+    user: 'dbmasteruser',
+    password: 'useiteasier',
     database: 'inventoryDB'
 });
 db.connect();
