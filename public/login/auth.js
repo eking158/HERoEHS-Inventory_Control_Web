664b29748028303fd88b7bd4704ea3393c223daf
quokkalover <yad05020@gmail.com>
2023-01-16 16:05:28 +0900

    7th add_username

--- public/login/auth.js
+++ public/login/auth.js
@@ -10,7 +10,7 @@ router.get('/login', function (request, response) {
     var html = template.HTML(title, `
             <h2>로그인</h2>
             <form action="/auth/login_process" method="post">
-            <p><input class="login" type="text" name="username" placeholder="아이디"></p>
+            <p><input class="login" type="text" name="userid" placeholder="아이디"></p>
             <p><input class="login" type="password" name="pwd" placeholder="비밀번호"></p>
             <p><input class="btn" type="submit" value="로그인"></p>
             </form>            
@@ -21,15 +21,15 @@ router.get('/login', function (request, response) {
 
 // 로그인 프로세스
 router.post('/login_process', function (request, response) {
-    var username = request.body.username;
-    var password = request.body.pwd;
-    if (username && password) {             // id와 pw가 입력되었는지 확인
+    const userid = request.body.userid;
+    const password = request.body.pwd;
+    if (userid && password) {             // id와 pw가 입력되었는지 확인
 
-        db.query('SELECT * FROM userTable WHERE username = ? AND password = ?', [username, password], function (error, results, fields) {
+        db.query('SELECT * FROM userTable WHERE userid = ? AND password = ?', [userid, password], function (error, results, fields) {
             if (error) throw error;
             if (results.length > 0) {       // db에서의 반환값이 있으면 로그인 성공
                 request.session.is_logined = true;      // 세션 정보 갱신
-                request.session.nickname = username;
+                request.session.nickname = userid;
                 request.session.save(function () {
                     response.redirect(`/`);
                 });
@@ -59,7 +59,8 @@ router.get('/register', function (request, response) {
     var html = template.HTML(title, `
     <h2>회원가입</h2>
     <form action="/auth/register_process" method="post">
-    <p><input class="login" type="text" name="username" placeholder="아이디"></p>
+    <p><input class="login" type="text" name="username" placeholder="이름"></p>
+    <p><input class="login" type="text" name="userid" placeholder="아이디"></p>
     <p><input class="login" type="password" name="pwd" placeholder="비밀번호"></p>    
     <p><input class="login" type="password" name="pwd2" placeholder="비밀번호 재확인"></p>
     <p><input class="btn" type="submit" value="제출"></p>
@@ -71,16 +72,17 @@ router.get('/register', function (request, response) {
 
 // 회원가입 프로세스
 router.post('/register_process', function (request, response) {
-    var username = request.body.username;
-    var password = request.body.pwd;
-    var password2 = request.body.pwd2;
+    const username = request.body.username;
+    const userid = request.body.userid;
+    const password = request.body.pwd;
+    const password2 = request.body.pwd2;
 
-    if (username && password && password2) {
+    if (userid && password && password2) {
 
-        db.query('SELECT * FROM userTable WHERE username = ?', [username], function (error, results, fields) { // DB에 같은 이름의 회원아이디가 있는지 확인
+        db.query('SELECT * FROM userTable WHERE userid = ?', [userid], function (error, results, fields) { // DB에 같은 이름의 회원아이디가 있는지 확인
             if (error) throw error;
             if (results.length <= 0 && password == password2) {     // DB에 같은 이름의 회원아이디가 없고, 비밀번호가 올바르게 입력된 경우 
-                db.query('INSERT INTO userTable (username, password) VALUES(?,?)', [username, password], function (error, data) {
+                db.query('INSERT INTO userTable (username, userid, password) VALUES(?,?,?)', [username, userid, password], function (error, data) {
                     if (error) throw error2;
                     response.send(`<script type="text/javascript">alert("회원가입이 완료되었습니다!");
                     document.location.href="/";</script>`);
